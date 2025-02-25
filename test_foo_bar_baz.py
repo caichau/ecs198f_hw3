8c0519f2e5f233d216dc6e0d767ff8be8a1d3a3c
cait <116609054+caichau@users.noreply.github.com>
2025-02-25 10:21:53 -0800

    added tests to test string length and the number of baz, bar, and foo in the string

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -11,17 +11,48 @@ from foo_bar_baz import foo_bar_baz
 
 #Add testcases Here
 
-# Nothing should be printed if input is 0.
 def test_input_zero():
-    assert foo_bar_baz(0) == ""
+    assert foo_bar_baz(0) == "" # Nothing should be printed if input is 0.
 
-# Nothing should be printed if input is negative.
+
 def test_input_negative():
-    assert foo_bar_baz(-6) == ""
+    assert foo_bar_baz(-6) == "" # Nothing should be printed if input is negative.
+
 
 def test_divisible_by_only_one_number():
     assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
 
+
 def test_divisible_by_both_numbers():
     assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
 
+
+def test_divisible_by_none():
+    assert foo_bar_baz(2) == "1 2"
+
+
+def test_str_length():
+    for i in range(50):
+        str = foo_bar_baz(i)
+        assert len(str) == i
+
+
+def test_word_counts():
+
+    for n in range(50): # Loop through range of n values. 
+        baz_count = 0
+        bar_count = 0
+        foo_count = 0
+        str = foo_bar_baz(n)
+        for j in range(n + 1): # For each n value, count the number of baz, bar, and foo in str.
+            if (n % 3 == 0) and (i % 5 == 0):
+                baz_count += 1
+            elif (n % 3 == 0):
+                foo_count += 1
+            elif (n % 5 == 0):
+                bar_count += 1
+        
+        # Check that there are correct number of baz, bar, and foos. 
+        assert baz_count == str.count("Baz")
+        assert bar_count == str.count("Bar")
+        assert foo_count == str.count("foo")
